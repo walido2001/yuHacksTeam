f7d2ea1260e294aa60286fa8615b0e029162b4db
Walid AlDari <78936295+walido2001@users.noreply.github.com>
2021-02-13 20:42:26 -0800

    WALID VERSION

--- flask_app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextField, BooleanField, TextAreaField, SubmitField
-class ContactForm(FlaskForm):
-    name = TextField("Name")
-    email = TextField("Email")
-    subject = TextField("Subject")
-    message = TextAreaField("Message")
-    submit = SubmitField("Send")
